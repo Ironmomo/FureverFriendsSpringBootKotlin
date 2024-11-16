642076e17db11aa966afc663303baaff35bae60f
Andrin Fassbind <andrin.fassbind@gmail.com>
2024-11-16 11:12:02 +0100

    add swagger ui documentation for AuthController

--- src/main/kotlin/org/example/fureverfriends/security/controller/AuthController.kt
+++ src/main/kotlin/org/example/fureverfriends/security/controller/AuthController.kt
@@ -1,5 +1,10 @@
 package org.example.fureverfriends.security.controller
 
+import io.swagger.v3.oas.annotations.media.Content
+import io.swagger.v3.oas.annotations.media.Schema
+import io.swagger.v3.oas.annotations.responses.ApiResponse
+import io.swagger.v3.oas.annotations.responses.ApiResponses
+import org.example.fureverfriends.dto.error.ErrorResponseDTO
 import org.example.fureverfriends.security.dto.AuthenticationRequest
 import org.example.fureverfriends.security.dto.AuthenticationResponse
 import org.example.fureverfriends.security.service.AuthenticationService
@@ -13,6 +18,16 @@ import org.springframework.web.bind.annotation.RestController
 class AuthController(
     private val authenticationService: AuthenticationService
 ) {
+
+    @ApiResponses(
+        value = [
+            ApiResponse(
+                responseCode = "403",
+                description = "Ungültige Anmeldedaten",
+                content = [Content(schema = Schema(implementation = ErrorResponseDTO::class))]
+            )
+        ]
+    )
     @PostMapping
     fun authenticate(
         @RequestBody authRequest: AuthenticationRequest
