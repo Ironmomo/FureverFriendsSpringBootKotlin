bc72b81cf97d08e798d9b1602a7b6f8f9e41b124
Andrin Fassbind <andrin.fassbind@gmail.com>
2024-11-16 11:10:57 +0100

    add swagger ui documentation for UserController

--- src/main/kotlin/org/example/fureverfriends/controller/user/UserController.kt
+++ src/main/kotlin/org/example/fureverfriends/controller/user/UserController.kt
@@ -1,5 +1,11 @@
 package org.example.fureverfriends.controller.user
 
+import io.swagger.v3.oas.annotations.media.Content
+import io.swagger.v3.oas.annotations.media.Schema
+import io.swagger.v3.oas.annotations.responses.ApiResponse
+import io.swagger.v3.oas.annotations.responses.ApiResponses
+import io.swagger.v3.oas.annotations.tags.Tag
+import org.example.fureverfriends.dto.error.ErrorResponseDTO
 import org.example.fureverfriends.dto.user.CreateUserRequestDTO
 import org.example.fureverfriends.dto.user.FoundUsersDTO
 import org.example.fureverfriends.service.user.UserService
@@ -15,10 +21,25 @@ import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/api/user")
+@Tag(name = "User Controller", description = "Operations related to users.")
 class UserController(
     private val userService: UserService
 ) {
 
+    @ApiResponses(
+        value = [
+            ApiResponse(
+                responseCode = "409",
+                description = "User already exists",
+                content = [Content(schema = Schema(implementation = ErrorResponseDTO::class))]
+            ),
+            ApiResponse(
+                responseCode = "406",
+                description = "Password requirements not met",
+                content = [Content(schema = Schema(implementation = ErrorResponseDTO::class))]
+            )
+        ]
+    )
     @PostMapping("/create")
     @ResponseStatus(CREATED)
     fun createUser(@RequestBody userRequest: CreateUserRequestDTO) {
